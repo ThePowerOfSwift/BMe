c95b37f3407701346e0a4e301ee7261f235d6547
satorusasozaki <sasozakisatoru@gmail.com>
2016-11-28 01:51:00 -0800

    Add progress hud

--- BMe/VideoCell.swift
+++ BMe/VideoCell.swift
@@ -9,38 +9,55 @@
 import UIKit
 import AVKit
 import AVFoundation
+import MBProgressHUD
 
 class VideoCell: UITableViewCell {
 
     static let identifier = "VideoCell"
     var player: AVPlayer?
-    
+    var playerItem: AVPlayerItem?
+    var hud: MBProgressHUD?
+    @IBOutlet weak var hudView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        // Set up AVPlayer
         player = AVPlayer()
-    }
-    override func prepareForReuse() {
-        //player = nil
-        print("Reused")
-    }
-    
-    func setupPlayer(playerItem: AVPlayerItem) {
-        player?.replaceCurrentItem(with: playerItem)
         player?.isMuted = true
         let playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = contentView.bounds
         contentView.layer.addSublayer(playerLayer)
         
-        player?.play()
+        // Show MBProgressHUD when loading video
+        hud = MBProgressHUD(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        hudView.addSubview(self.hud!)
+        player?.addObserver(self, forKeyPath: "status", options: [], context: nil)
+
+        
     }
     
-    
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override func prepareForReuse() {
+        print("Reused")
     }
     
+    // Replace player item in player
+    func setupPlayer(playerItem: AVPlayerItem) {
+        
+        self.hud?.show(animated: true)
+        // This way, AVPlayer won't overlap when reused
+        player?.replaceCurrentItem(with: playerItem)
+
+    }
+    
+    // http://stackoverflow.com/questions/5401437/knowing-when-avplayer-object-is-ready-to-play
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "status" {
+            if self.player?.status == AVPlayerStatus.readyToPlay {
+                self.hud?.hide(animated: true)
+                //self.player?.play()
+            }
+        }
+    }
+
 }
