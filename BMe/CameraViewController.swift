ebb9799ee99c14e462ec62a5d02870d160917759
Jonathan James <gococo@gmail.com>
2016-12-01 17:24:46 -0800

    Added camera add text layer

--- BMe/CameraViewController.swift
+++ BMe/CameraViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: - Outlets
     @IBOutlet weak var cameraControlView: UIView!
@@ -19,16 +19,26 @@ class CameraViewController: UIViewController {
     }
     
     //MARK:- Model
-    var textFields: [UITextField] = []
+    var getTextFieldInView: [UITextField] {
+        get {
+            var textFields: [UITextField] = []
+            for view in cameraControlView.subviews {
+//grab textfields
+            }
+            return textFields
+        }
+    }
     
     //MARK:- Variables
-    
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tappedCamerView(_:)))
+        cameraControlView.addGestureRecognizer(tap)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,32 +46,73 @@ class CameraViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: - Methods
-    
+    //MARK: - Manging Textfeld methods
+   
+    // Add a next text field to the screen
     func addTextFieldToView() {
+        // Create new textfield
         let textField = UITextField()
+        textField.delegate = self
+
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .sentences
+        textField.spellCheckingType = .no
+        textField.keyboardType = .asciiCapable
+        textField.returnKeyType = .done
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(tappedTextField(_:)))
+        // Add didedit event notifier
+        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControlEvents.editingChanged)
+        // Add double tap (to delete)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTappedTextField(_:)))
+        tap.numberOfTapsRequired = 2
         textField.addGestureRecognizer(tap)
+        // Add pan (to move)
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(pannedTextField(_:)))
+        textField.addGestureRecognizer(pan)
         
+        // Default appearance
+        textField.placeholder = "T"
+        textField.sizeToFit()
         
-        textFields.append(textField)
+        textField.frame.origin = cameraControlView.center
+        
+        cameraControlView.addSubview(textField)
+        textField.becomeFirstResponder()
     }
     
-    func tappedTextField(_ sender: UITapGestureRecognizer) {
+    // On change resize the view
+    func textFieldDidChange(_ sender: UITextField) {
+        sender.sizeToFit()
+    }
+    
+    // On double tap remove the textfield
+    func doubleTappedTextField(_ sender: UITapGestureRecognizer) {
         let textField = sender.view
         textField?.removeFromSuperview()
     }
     
-    func getTextFieldInView() -> [UITextField] {
-        var textFields: [UITextField] = []
-        for view in cameraControlView.subviews {
-            if isKind(of: UITextField.self) {
-                let textfield = view
-                textFields.append(textfield as! UITextField)
-            }
+    // On pan move the textfield
+    func pannedTextField(_ sender: UIPanGestureRecognizer) {
+        if let textField = sender.view {
+        
+            let point = sender.location(in: cameraControlView)
+            textField.frame.origin = point
         }
-        return textFields
     }
     
+    //MARK: - Textfield Delegate & FirstResponder methods
+    // Tapped on background: end editing on all textfields
+    func tappedCamerView(_ sender: UITapGestureRecognizer) {
+        cameraControlView.endEditing(true)
+    }
+    
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
