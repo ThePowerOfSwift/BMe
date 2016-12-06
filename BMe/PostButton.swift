f96978a3c05637dc9794bab323a7111e6ad7179c
Jonathan James <gococo@gmail.com>
2016-12-06 14:09:24 -0800

    updated appearance

--- BMe/PostButton.swift
+++ BMe/PostButton.swift
@@ -17,18 +17,20 @@ class PostButton: UIButton {
     
     func setup() {
         setImageDefault()
+        clipsToBounds = false
+        
+        // Programatically makes border
+        // Make enclosing view with borders
+        let borderBuffer: CGFloat = 10
+        let borderView = UIView(frame: bounds.insetBy(dx: -borderBuffer, dy: -borderBuffer))
+        borderView.layer.borderWidth = 4
+        borderView.layer.cornerRadius = 10
+        borderView.layer.borderColor = Styles.Color.Tertiary.cgColor
+        addSubview(borderView)
     }
     
     func setImageDefault() {
-        // TODO: - CHANGE TO WHITE
-        let defaultImage = UIImage(named: Constants.Images.hook)
+        let defaultImage = UIImage(named: Constants.Images.hookYellow)
         setImage(defaultImage, for: UIControlState.normal)
     }
-    
-    func setImageYellow() {
-//        let defaultImage = UIImage(named: Constants.Images.crossYellow)
-//        setImage(defaultImage, for: UIControlState.normal)
-    }
-
-
 }
