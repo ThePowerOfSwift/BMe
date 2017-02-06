6cadc607212b0ff4b3edc2eeaf8858382933de9e
Jonathan James <gococo@gmail.com>
2017-02-06 10:33:12 -0800

    Separated JSONStack to its own file

---
+++ BMe/JSONStack.swift
@@ -0,0 +1,73 @@
+//
+//  JSONStack.swift
+//  BMe
+//
+//  Created by Jonathan Cheng on 2/6/17.
+//  Copyright © 2017 Jonathan Cheng. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+/**
+ Class that creates and manages a 'queue' in JSON given any child in JSON tree
+ Queue is stored in sub path: ~/queue
+ Count of the queue is maintained by adding and removing timestamped children under /queue/count
+ */
+class JSONStack: NSObject {
+    /**
+     Queue object onto JSON stack
+     */
+    class func queue(object: [String: AnyObject?], database: FIRDatabaseReference) {
+        // Add object to stack
+        // Path: ~/queue
+        database.child(keys.object).updateChildValues(object)
+    }
+    
+    /**
+     Pop next object from JSON stack FIFO
+     */
+    class func popFIFO(database: FIRDatabaseReference, completion:@escaping (FIRDataSnapshot?)->()) {
+        // append "queue" to JSON path
+        let database = database.child(keys.object)
+        database.queryOrderedByKey().observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            // test if empty (then return nil)
+            if (snapshot.childrenCount < 1) || (!snapshot.exists()) {
+                completion(nil)
+            } else { // otherwise pop from queue and send to handler
+                // get first child
+                var objectToPop: FIRDataSnapshot?
+                for snapChild in snapshot.children {
+                    if let snapChild = snapChild as? FIRDataSnapshot {
+                        objectToPop = snapChild
+                    }
+                    break
+                }
+                if let objectToPop = objectToPop {
+                    // Get key ID
+                    let key = objectToPop.key
+                    
+                    // pop from queue & return
+                    database.child(key).removeValue()
+                    completion(objectToPop)
+                } else {
+                    print("Error, tried to pop item off queue; resulted in nil error")
+                }
+            }
+        })
+    }
+    
+    /**
+     Count the number of items in queue.  Implemented by counting the number of children under /queue
+     */
+    class func count(database: FIRDatabaseReference, completion:@escaping (UInt)->()) {
+        database.child(keys.object).observeSingleEvent(of: .value, with: { (snapshot) in
+            completion(snapshot.childrenCount)
+        })
+    }
+    
+    struct keys {
+        static var object = "queue"
+    }
+}
--- BMe/VoteBooth.swift
+++ BMe/VoteBooth.swift
@@ -11,14 +11,7 @@ import FirebaseDatabase
 
 class VoteBooth: NSObject {
 
-    struct Key {
-        static var object = "votebooth"
-        static var timestamp = "timestamp"
-        static var contentType = "contentType"
-        static var matchup = "matchup"
-        static var posts = "posts"
-        static var votes = "votes"
-    }
+   
     
     struct Matchup {
         let ID: String
@@ -27,8 +20,8 @@ class VoteBooth: NSObject {
     }
     
     static var database: FIRDatabaseReference = FIRManager.shared.database
-    static var imageQueue = database.child(Key.object)
-    static var matchupQueue = database.child(Key.object).child(Key.matchup)
+    static var imageQueue = database.child(keys.object)
+    static var matchupQueue = database.child(keys.object).child(keys.matchup)
     
     /**
      Submit a photo for voting
@@ -37,13 +30,13 @@ class VoteBooth: NSObject {
         
         // construct queue data to push
         // queue/(ID)/(metadata)
-        let metadata = [Key.contentType : ContentType.post.string() as AnyObject,
-                        Key.timestamp: Date().toString() as AnyObject]
-        let object = [ID : metadata]
+        let metadata = [keys.contentType : ContentType.post.string() as AnyObject,
+                        keys.timestamp: Date().toString() as AnyObject]
+        let object = [ID: metadata as AnyObject]
         
         // add to (database) queue
         // ~/votebooth
-        JSONStack.queue(object: object as AnyObject , database: VoteBooth.imageQueue)
+        JSONStack.queue(object: object, database: VoteBooth.imageQueue)
         
         // Update matchup bucket
         VoteBooth.update()
@@ -92,8 +85,8 @@ class VoteBooth: NSObject {
             // return matchup struct/object
             if let snap = snap {
                 let data = snap.value as! [String: AnyObject?]
-                let timestamp = data[Key.timestamp] as! String
-                let posts = data[Key.posts] as! [String: AnyObject?]
+                let timestamp = data[keys.timestamp] as! String
+                let posts = data[keys.posts] as! [String: AnyObject?]
                 
                 let matchup = Matchup(ID: snap.key, timestamp: timestamp, posts: posts)
                 
@@ -107,9 +100,9 @@ class VoteBooth: NSObject {
      */
     class func result(matchID: String, winnerID: String) {
         
-        let meta = [Key.timestamp: Date().toString()]
+        let meta = [keys.timestamp: Date().toString()]
         // votebooth/matchup/(key)/posts/(winner key)/votes/
-        VoteBooth.matchupQueue.child(matchID).child(Key.posts).child(winnerID).child(Key.votes).child(UserAccount.currentUser.uid!).setValue(meta)
+        VoteBooth.matchupQueue.child(matchID).child(keys.posts).child(winnerID).child(keys.votes).child(UserAccount.currentUser.uid!).setValue(meta)
     }
     
     /** 
@@ -119,8 +112,8 @@ class VoteBooth: NSObject {
         // Create a matchup and insert into matchup bucket
         // ~/matchup/(key)/posts/(first.key)/etc
         let posts = [first.key: first.value, second.key: second.value]
-        let metadata: [String: Any] = [Key.timestamp: Date().toString(),
-                                       Key.posts: posts]
+        let metadata: [String: Any] = [keys.timestamp: Date().toString(),
+                                       keys.posts: posts]
 
         // add to (database) queue
         VoteBooth.matchupQueue.childByAutoId().setValue(metadata)
@@ -129,73 +122,17 @@ class VoteBooth: NSObject {
     class func remove(_ matchID: String) {
         VoteBooth.matchupQueue.child(matchID).removeValue()
     }
+    
+    struct keys {
+        static var object = "votebooth"
+        static var timestamp = "timestamp"
+        static var contentType = "contentType"
+        static var matchup = "matchup"
+        static var posts = "posts"
+        static var votes = "votes"
+    }
 }
 
-// Move to FIRManager
-/** 
- Class that creates and manages a 'queue' in JSON
- Queue is stored in a sub path /queue
- Count of the queue is maintained by adding and removing timestamped children under /queue/count
- */
-class JSONStack {
-    
-    struct Key {
-        static var object = "queue"
-        static var timestamp = "timestamp"
-    }
-    
-    /**
-     Queue object onto JSON stack
-     */
-    class func queue(object: AnyObject, database: FIRDatabaseReference) {
-        // Add object to stack
-        // Path: ~/queue
-        database.child(Key.object).updateChildValues(object as! [AnyHashable : Any])
-    }
-    
-    /**
-     Pop next object from JSON stack FIFO
-     */
-    class func popFIFO(database: FIRDatabaseReference, completion:@escaping (FIRDataSnapshot?)->()) {
-        // append "queue" to JSON path
-        let database = database.child(Key.object)
-        database.queryOrderedByKey().observeSingleEvent(of: .value, with: { (snapshot) in
-            
-            // test if empty (then return nil)
-            if (snapshot.childrenCount < 1) || (!snapshot.exists()) {
-                completion(nil)
-            } else { // otherwise pop from queue and send to handler
-                // get first child
-                var objectToPop: FIRDataSnapshot?
-                for snapChild in snapshot.children {
-                    if let snapChild = snapChild as? FIRDataSnapshot {
-                        objectToPop = snapChild
-                    }
-                    break
-                }
-                if let objectToPop = objectToPop {
-                    // Get key ID
-                    let key = objectToPop.key
-                
-                    // pop from queue & return
-                    database.child(key).removeValue()
-                    completion(objectToPop)
-                } else {
-                    print("Error, tried to pop item off queue; resulted in nil error")
-                }
-            }
-        })
-    }
-    
-    /** 
-     Count the number of items in queue.  Implemented by counting the number of children under /queue
-     */
-    class func count(database: FIRDatabaseReference, completion:@escaping (UInt)->()) {
-        database.child(Key.object).observeSingleEvent(of: .value, with: { (snapshot) in
-            completion(snapshot.childrenCount)
-        })
-    }
-}
 
 
 
