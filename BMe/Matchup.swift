acff840c3811eea023bedd924db3767f85d617f0
Jonathan James <gococo@gmail.com>
2017-02-06 22:57:33 -0800

    Updated JSONStack

---
+++ BMe/Matchup.swift
@@ -0,0 +1,13 @@
+//
+//  Matchup.swift
+//  BMe
+//
+//  Created by Jonathan Cheng on 2/6/17.
+//  Copyright © 2017 Jonathan Cheng. All rights reserved.
+//
+
+import UIKit
+
+class Matchup: JSONObject {
+
+}
