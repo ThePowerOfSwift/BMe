cd71239cb1db7cc533a942a518e7aec19cbb3b5e
satorusasozaki <sasozakisatoru@gmail.com>
2017-02-22 17:54:05 -0800

    Add plain filter

--- Filter.swift
+++ Filter.swift
@@ -8,30 +8,38 @@ import UIKit
 
 class Filter: NSObject {
     var name: String
-    var filter: CIFilter
+    var filter: CIFilter?
     var imageUrlString: String
 
-    init(name: String, filter: CIFilter, imageUrlString: String) {
+    init(name: String, filter: CIFilter?, imageUrlString: String) {
         self.name = name
         self.filter = filter
         self.imageUrlString = imageUrlString
     }
 
     func generateFilteredCIImage(sourceImage: CIImage) -> CIImage? {
-        filter.setValue(sourceImage, forKey: kCIInputImageKey)
-        return filter.outputImage
+        if let filter = filter {
+            filter.setValue(sourceImage, forKey: kCIInputImageKey)
+            return filter.outputImage
+        } else {
+            return sourceImage
+        }
     }
 
     class func list() -> [Filter] {
         // Double size for testing
-        return [Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone")!, imageUrlString: "alcatraz.jpg"),
-                Filter(name: "False", filter: CIFilter(name: "CIFalseColor")!, imageUrlString: "chinatown.jpg"),
-                Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone")!, imageUrlString: "alcatraz.jpg"),
-                Filter(name: "False", filter: CIFilter(name: "CIFalseColor")!, imageUrlString: "chinatown.jpg"),
-                Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone")!, imageUrlString: "alcatraz.jpg"),
-                Filter(name: "False", filter: CIFilter(name: "CIFalseColor")!, imageUrlString: "chinatown.jpg"),
-                Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone")!, imageUrlString: "alcatraz.jpg"),
-                Filter(name: "False", filter: CIFilter(name: "CIFalseColor")!, imageUrlString: "chinatown.jpg")
+        return [Filter(name: "Nothing", filter: nil, imageUrlString: "montgomery.jpg"),
+                Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone"), imageUrlString: "alcatraz.jpg"),
+                Filter(name: "False", filter: CIFilter(name: "CIFalseColor"), imageUrlString: "chinatown.jpg"),
+                Filter(name: "Nothing", filter: nil, imageUrlString: "montgomery.jpg"),
+                Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone"), imageUrlString: "alcatraz.jpg"),
+                Filter(name: "False", filter: CIFilter(name: "CIFalseColor"), imageUrlString: "chinatown.jpg"),
+                Filter(name: "Nothing", filter: nil, imageUrlString: "montgomery.jpg"),
+                Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone"), imageUrlString: "alcatraz.jpg"),
+                Filter(name: "False", filter: CIFilter(name: "CIFalseColor"), imageUrlString: "chinatown.jpg"),
+                Filter(name: "Nothing", filter: nil, imageUrlString: "montgomery.jpg"),
+                Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone"), imageUrlString: "alcatraz.jpg"),
+                Filter(name: "False", filter: CIFilter(name: "CIFalseColor"), imageUrlString: "chinatown.jpg")
         ]
     }
 }
